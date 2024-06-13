0327850bfbe4b9e869cd11b2f4ca2d2acbcc21b4
Partial <jason@wridgway.net>
2024-06-13 11:59:13 +1000

    Remove unnecissary mod layer; normal mode covers this fuctionality

--- keyboards/bored/keymaps/cultofpartiality/keymap.c
+++ keyboards/bored/keymaps/cultofpartiality/keymap.c
@@ -18,7 +18,6 @@ enum planck_keycodes {
 
 //Layer key defines
 #define NUMPAD MO(_NUMPAD)
-#define RAISE2 LT(_NAV,KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -36,14 +35,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `---------'-----------------------------------------------------------------------------------'---------'
     */
     [_QWERTY] = LAYOUT_ortho_5x14(
-    QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /**/ _______, _______, /**/ KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    /**/ KC_PSCR, _______, /**/ KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    /**/ KC_DEL,  _______, /**/ KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    /**/ KC_F5,   _______, /**/ KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, NUMPAD,  KC_SYMB, NUM_SPC, /**/ KC_ENT,  KC_BSPC, /**/ NAV_ENT, KC_NAV,   KM_CSHT, KC_RALT, KC_RGUI, KC_RCTL
-    ),
-
-    [_MOD] = LAYOUT_ortho_5x14(
     QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /**/ _______, _______, /**/ KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    /**/ KC_PSCR, _______, /**/ KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
     KC_TAB,  HM_A,    HM_S,    HM_D,    HM_F,    KC_G,    /**/ KC_DEL,  _______, /**/ KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_QUOT,
