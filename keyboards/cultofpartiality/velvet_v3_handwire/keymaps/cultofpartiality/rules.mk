bb5bc37a46e80b6a2b4fb3215d8838bc47c48b63
Partial <jason@wridgway.net>
2025-07-31 23:05:37 +1000

    Velvet handwire

---
+++ keyboards/cultofpartiality/velvet_v3_handwire/keymaps/cultofpartiality/rules.mk
@@ -0,0 +1,7 @@
+RGBLIGHT_ENABLE = no
+OLED_DRIVER_ENABLE = no
+OLED_ENABLE = no           # OLED display
+EXTRAKEY_ENABLE = yes # Audio control and System control
+
+COMBO_ENABLE = yes
+LEADER_ENABLE = yes
