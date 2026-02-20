db140ad7231d454dc987f5e57e2236ab919745bd
Partial <jason@wridgway.net>
2026-02-20 21:02:27 +1100

    Added to non-mac layer
    Why do I even have a mac layer?

--- keyboards/keychron/q65/keymaps/cultofpartiality/keymap.c
+++ keyboards/keychron/q65/keymaps/cultofpartiality/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT_ansi_73(
         KC_MUTE, QK_GESC, KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_DEL,
-        _______, KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_HOME,
+        NM_TG,   KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_HOME,
         _______, KC_TAB,  HM_A,     HM_S,     HM_D,    HM_F,    KC_G,    KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN,  KC_QUOT,            KC_ENT,           KC_PGUP,
         _______, KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT, KC_UP,   KC_PGDN,
         _______, KC_LCTL, KC_LWIN,  KC_LALT,                             NAV_SPC,                            KC_RALT,  MO(_FN2), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT),
