3e688b3bba3cad5e84dca2788d1dd975b24b6f63
Partial <jason@wridgway.net>
2026-07-04 23:49:33 +1000

    Added fold/unfold combos. Setup for vscode so far

--- users/cultofpartiality/combos.c
+++ users/cultofpartiality/combos.c
@@ -1,4 +1,4 @@
-//See "https://github.com/qmk/qmk_firmware/issues/21137" for info around needed this file
+// See "https://github.com/qmk/qmk_firmware/issues/21137" for info around needed this file
 
 #pragma once
 
@@ -7,39 +7,58 @@
 
 /*Combos*/
 const uint16_t PROGMEM combo_homerowShiftsCapsWord[] = {HM_F, HM_J, COMBO_END};
-const uint16_t PROGMEM combo_leaderKey2[] = {HM_S, HM_D, COMBO_END};
-const uint16_t PROGMEM combo_homerowTab[] = {HM_A, HM_S, COMBO_END};
-const uint16_t PROGMEM combo_addDelete[] = {KC_P, KC_BSPC, COMBO_END};
-const uint16_t PROGMEM combo_F5[] = {KC_R, KC_T, COMBO_END};
-const uint16_t PROGMEM combo_altF4[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM combo_leaderKey2[]            = {HM_S, HM_D, COMBO_END};
+const uint16_t PROGMEM combo_homerowTab[]            = {HM_A, HM_S, COMBO_END};
+const uint16_t PROGMEM combo_addDelete[]             = {KC_P, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM combo_F5[]                    = {KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM combo_altF4[]                 = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM combo_fold[]                  = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_unfold[]                = {KC_U, KC_O, COMBO_END};
 
-
- /*----------------------------------------------------------------*\
- |      Combos - Tap multiple keys to trigger an action             |
- \*----------------------------------------------------------------*/
- //There's difficult moving this to the userspace... Lets see what we can get away with
+/*----------------------------------------------------------------*\
+|      Combos - Tap multiple keys to trigger an action             |
+\*----------------------------------------------------------------*/
+// There's difficult moving this to the userspace... Lets see what we can get away with
 
 combo_t key_combos[] = {
     COMBO_ACTION(combo_homerowShiftsCapsWord),
     COMBO_ACTION(combo_altF4),
-    COMBO(combo_leaderKey2,QK_LEAD),
-    COMBO(combo_homerowTab,KC_TAB),
+    COMBO_ACTION(combo_fold),
+    COMBO_ACTION(combo_unfold),
+    COMBO(combo_leaderKey2, QK_LEAD),
+    COMBO(combo_homerowTab, KC_TAB),
     COMBO(combo_addDelete, KC_DEL),
     COMBO(combo_F5, KC_F5),
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
-  switch(combo_index) {
-    case 0: //combo_homerowShiftsCapsWord
-      if (pressed)
-        trigger_caps_funcs();
-      break;
-    case 1: //combo_altF4
-      if (pressed && (get_mods() & MOD_BIT(KC_RCTL))) {
-        unregister_code(KC_RCTL);
-        tap_code16(A(KC_F4));
-        register_code(KC_RCTL);
-      }
-      break;
-  }
+    switch (combo_index) {
+        case 0: // combo_homerowShiftsCapsWord
+            if (pressed) trigger_caps_funcs();
+            break;
+        case 1: // combo_altF4
+            if (pressed) {
+                if (get_mods() & MOD_BIT(KC_RCTL)) {
+                    unregister_code(KC_RCTL);
+                    tap_code16(A(KC_F4));
+                    register_code(KC_RCTL);
+                }
+                else if(get_mods() & MOD_BIT(KC_LCTL)) {
+                    unregister_code(KC_LCTL);
+                    tap_code16(A(KC_F4));
+                    register_code(KC_LCTL);
+                }
+            }
+            break;
+        case 2: // combo_fold
+            if (pressed) {
+                tap_code16(C((S(KC_LBRC))));
+            }
+            break;
+        case 3: // combo_unfold
+            if (pressed) {
+                tap_code16(C((S(KC_RBRC))));
+            }
+            break;
+    }
 }
