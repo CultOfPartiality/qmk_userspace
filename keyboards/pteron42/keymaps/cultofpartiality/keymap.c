50d3444402440bbfad5a25b7fa8db83ee8a68d4a
cultofpartiality <jason@wridgway.net>
2026-07-09 21:49:13 +1000

    Added an alt for playing Opus Magnus in QMK normal mode

--- keyboards/pteron42/keymaps/cultofpartiality/keymap.c
+++ keyboards/pteron42/keymaps/cultofpartiality/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      /**/                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  HM_A,    HM_S,    HM_D,    HM_F,    KC_G,                      /**/                   KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, HM_QUOT,
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      /**/                   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
-                                            KC_1,    SYM_ESC, NUM_SPC, MOU_ENT, /**/ KC_BSPC, NAV_ENT, NAV_DEL, KC_2
+                                            KC_LALT, SYM_ESC, NUM_SPC, MOU_ENT, /**/ KC_BSPC, NAV_ENT, NAV_DEL, KC_2
     ),
 
     [_NAV]    = LAYOUT_split_3x6_4_wrapper(
